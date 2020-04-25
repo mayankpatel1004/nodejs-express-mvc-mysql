da4b0503da22a19b029daba231043b2599da100b
Mayank Patel <mayankp@digiflux.io>
2020-04-25 23:39:26 +0530

    pdf changes updated

--- api/users/user.service.js
+++ api/users/user.service.js
@@ -55,7 +55,7 @@ module.exports = {
     },
     excelRead: callBack => {
         var xlsx = require("xlsx");
-        var wb = xlsx.readFile('/home/digiflux/Documents/work/practice/mayank-practice/uploads/rex.xlsx',{cellDates:true});
+        var wb = xlsx.readFile('uploads/sample.xlsx',{cellDates:true});
         var ws = wb.Sheets["Rex-Liquor-Data"];
         var data = xlsx.utils.sheet_to_json(ws);
         var newData = data.map(function(record){
@@ -102,13 +102,54 @@ module.exports = {
         );
     },
     exportUsersPdf: callBack => {
-        const PDFDocument = require('pdfkit');
-        const fs = require('fs');
-        const doc = new PDFDocument();
-        const content = '<table><tr><th>No.</th><th>Name</th><th>Number</th><th>Status</th></tr><tr><td>1</td><td>Mayank</td><td>23</td><td>Active</td></tr></table>';
+        //const PDFDocument = require('pdfkit');
+        //const fs = require('fs');
+        // const doc = new PDFDocument();
+        //const content = '<table><tr><th>No.</th><th>Name</th><th>Number</th><th>Status</th></tr><tr><td>1</td><td>Mayank</td><td>23</td><td>Active</td></tr></table>';
+        // doc.pipe(fs.createWriteStream('uploads/users.pdf'));
+        // doc.fontSize(20).text(content);
+        // doc.end();
+        var PDFDocument = require('pdfkit');
+        var fs=require('fs');
+        doc = new PDFDocument();
+        x1=doc.x;
+        x6=310;
+        doc.rect(doc.x, doc.y, 450, 65)
+        .moveTo(300, doc.y).lineTo(300, doc.y+65)
+        .moveDown(0.2)
+        
+        .text('Member Name',{indent:5, align:'left',width:140, height:doc.currentLineHeight()})
+        .rect(x1,doc.y,450,0.5)
+        .moveUp()
+        .text("Mayank",x6,doc.y)
+        .moveDown(0.2)
+        
+        .text('Member ID',x1,doc.y,{indent:5, align:'left',width:140, height:doc.currentLineHeight()})
+        .rect(x1,doc.y,450,0.5)
+        .moveUp()
+        .text("1",x6,doc.y)
+        .moveDown(0.2)
+        
+        .text('Member DOB',x1,doc.y,{indent:5, align:'left'})
+        .rect(x1,doc.y,450,0.5)
+        .moveUp()
+        .text("2020-11-11",x6,doc.y)
+        .moveDown(0.2)
+        
+        .text('Baby Due Date',x1,doc.y,{indent:5, align:'left'})
+        .moveUp()
+        .text("2020-11-11",x6,doc.y)
+        .stroke()
+        .moveDown(1.5);
+        
         doc.pipe(fs.createWriteStream('uploads/users.pdf'));
-        doc.fontSize(20).text(content);
         doc.end();
+
+
+
+
+
+        
         return callBack(null, {});
     },
     getUserByUserId: (id, callBack) => {
